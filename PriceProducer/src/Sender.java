4982e8400639afa12b9089ba8841ca175cfa167b
WeiChen <droyuki@gmail.com>
2015-06-13 02:08:54 +0800

    Ok.

--- PriceProducer/src/Sender.java
+++ PriceProducer/src/Sender.java
@@ -8,7 +8,10 @@ class Producer extends Thread {
             while (true) {
                 Random rand = new Random();
                 int random = rand.nextInt(1000)+1000;
-                String sendMe = String.valueOf(random);
+                String sendMe = "msgKey:taiwan_future_TFM5_1m@1434087245764;8:";
+                sendMe += String.valueOf(random);
+                sendMe += ";5:1192.2;7:1192.2;Date:1434087245764;9:50866.0;6:1192.6;KBAR:taiwan_future_TFM5_1m_1434087245764;";
+                System.out.println(sendMe);
                 KafkaTopicProducer.getInstance().send("PriceTest", sendMe);
                 Thread.sleep(500);
             }
