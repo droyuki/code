4b3b90f7f04156b5d2550676d6f9dbc3cc359e29
WeiChen <droyuki@gmail.com>
2015-06-14 11:14:07 +0800

    Add args.

--- LinearRegression/src/kafka/consumer/Start.java
+++ LinearRegression/src/kafka/consumer/Start.java
@@ -5,8 +5,9 @@ public class Start{
     //-Djava.library.path="/home/kmlab/R/x86_64-pc-linux-gnu-library/3.0/rJava/jri"
     public static void main(String[] args) {
         Receiver r = new Receiver();
+        String groupid = args[0], topic = args[1];
         @SuppressWarnings("unused")
         KafkaTopicConsumer ktc = new KafkaTopicConsumer("192.168.1.104:2181,192.168.1.105:2181,192.168.1.106:2181",
-                "PriceTester", "PriceTest", r);
+                groupid, topic, r);
     }
 }
