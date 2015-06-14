85b94477d0fba46e64a8fdc95b90809dee8a4e84
WeiChen <droyuki@gmail.com>
2015-06-14 11:36:17 +0800

    Add args.

--- LinearRegression/src/kafka/consumer/Start.java
+++ LinearRegression/src/kafka/consumer/Start.java
@@ -6,6 +6,10 @@ public class Start{
     public static void main(String[] args) {
         Receiver r = new Receiver();
         String groupid = args[0], topic = args[1];
+        if(args.length != 2) {
+            System.out.println("Please enter groupid and topic.");
+            System.exit(0);
+        }
         @SuppressWarnings("unused")
         KafkaTopicConsumer ktc = new KafkaTopicConsumer("192.168.1.104:2181,192.168.1.105:2181,192.168.1.106:2181",
                 groupid, topic, r);
